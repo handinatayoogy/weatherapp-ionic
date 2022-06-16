12dd5c6508bb840a96d9d38c00f6d44fc159ca58
Yoogy Handi Nata <72843811+handinatayoogy@users.noreply.github.com>
2022-06-16 10:43:25 +0700

    initial commit

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  url = 'https://api.openweathermap.org/data/2.5/';
+  key = 'd5df15165a2082af2e543be4d669d349';
+  city = 'Banjarnegara';
+
+  constructor(private http: HttpClient) {}
+
+  getDataNow() {
+    return this.http.get(
+      `${this.url}weather?q=${this.city}&appid=${this.key}&units=metric`
+    );
+  }
+
+  getDataForecast() {
+    return this.http.get(
+      `${this.url}forecast?q=${this.city}&appid=${this.key}&units=metric`
+    );
+  }
+}
